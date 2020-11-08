2252df5ed23f12b47c20819d8b50d48e84387752
Jamie <mostsimply@gmail.com>
2020-11-08 19:47:32 +0900

    Update

---
+++ _site/js/main.js
@@ -0,0 +1,33 @@
+(function () {
+	var header = document.getElementById("mainHeader");
+
+	function changeHeader() {
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+	}
+
+	var didScroll = false;
+
+	$(window).scroll(function () {
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			didScroll = false;
+			changeHeader();
+		}
+	}, 100);
+
+	changeHeader();
+
+	$("a[href*=\\#]").on("click", function (event) {
+		if(this.pathname === window.location.pathname) {
+			event.preventDefault();
+
+			$("html, body").animate({
+				scrollTop: $(this.hash).offset().top
+			}, 500);
+		}
+	});
+})();
